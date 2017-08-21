2dddaece1b2f9ea00728c3c56896f5dba47c478d
Brent Long <blong123@gmail.com>
2017-08-21 11:30:56 -0700

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(3003, function () {
+  console.log('Example app listening on port 3003!')
+})
+
